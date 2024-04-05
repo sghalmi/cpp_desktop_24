bd9daca7d3f91f4671124a92c192edfb8882acbd
Sarah Ghalmi <123117781+sghalmi@users.noreply.github.com>
2024-04-05 14:36:05 -0400

    Add files via upload

---
+++ js/pet.js
@@ -0,0 +1,66 @@
+function displayPets1(){
+    var pet_1 = document.getElementById('pet1');
+    var displaySetting = pet_1.style.display;
+
+    if (displaySetting == "none"){
+        pet_1.style.display = "block";
+    }
+    else{
+        pet_1.style.display = "none";
+    }
+}
+function displayPets2(){
+    var pet_1 = document.getElementById('pet2');
+    var displaySetting = pet_1.style.display;
+
+    if (displaySetting == "none"){
+        pet_1.style.display = "block";
+    }
+    else{
+        pet_1.style.display = "none";
+    }
+}
+function displayPets3(){
+    var pet_1 = document.getElementById('pet3');
+    var displaySetting = pet_1.style.display;
+
+    if (displaySetting == "none"){
+        pet_1.style.display = "block";
+    }
+    else{
+        pet_1.style.display = "none";
+    }
+}
+function displayPets4(){
+    var pet_1 = document.getElementById('pet4');
+    var displaySetting = pet_1.style.display;
+
+    if (displaySetting == "none"){
+        pet_1.style.display = "block";
+    }
+    else{
+        pet_1.style.display = "none";
+    }
+}
+function displayPets5(){
+    var pet_1 = document.getElementById('pet5');
+    var displaySetting = pet_1.style.display;
+
+    if (displaySetting == "none"){
+        pet_1.style.display = "block";
+    }
+    else{
+        pet_1.style.display = "none";
+    }
+}
+function displayPets6(){
+    var pet_1 = document.getElementById('pet6');
+    var displaySetting = pet_1.style.display;
+
+    if (displaySetting == "none"){
+        pet_1.style.display = "block";
+    }
+    else{
+        pet_1.style.display = "none";
+    }
+}
---
+++ js/recipe.js
@@ -0,0 +1,34 @@
+function displayRecipes1(){
+    var rec_1 = document.getElementById('recipe1');
+    var displaySetting = rec_1.style.display;
+
+    if (displaySetting == "none"){
+        rec_1.style.display = "block";
+    }
+    else{
+        rec_1.style.display = "none";
+    }
+}
+
+function displayRecipes2(){
+    var rec_1 = document.getElementById('recipe2');
+    var displaySetting = rec_1.style.display;
+
+    if (displaySetting == "none"){
+        rec_1.style.display = "block";
+    }
+    else{
+        rec_1.style.display = "none";
+    }
+}
+function displayRecipes3(){
+    var rec_1 = document.getElementById('recipe3');
+    var displaySetting = rec_1.style.display;
+
+    if (displaySetting == "none"){
+        rec_1.style.display = "block";
+    }
+    else{
+        rec_1.style.display = "none";
+    }
+}
